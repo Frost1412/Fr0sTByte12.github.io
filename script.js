e7ea4543538551d040ffa605a087f37eb6404830
Fr0sTByte12 <Nguyenkimdinh1412@gmail.com>
2023-10-19 01:06:57 +0700

    lmao 2

---
+++ script.js
@@ -0,0 +1,10 @@
+function expandmenu() {
+    const expandmenu = document.getElementById("hambuler");
+    if(expandmenu.style.display == "block"){
+        expandmenu.style.display="none";
+
+    }
+    else {
+        expandmenu.style.display="block";
+    }
+} 
